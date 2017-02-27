2d11c1668bd7c7b88815b35ae60f0ed7f8df2555
ycg <ytu_yangcg@163.com>
2017-02-27 23:30:20 +0800

    Site updated: 2017-02-27 23:30:19

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@
 
         var bannerNode = $('.top-image');
         if(bannerNode.data('enable')){
-            var index = parseInt((Math.random() * 12) + 1);
+            var index = parseInt((Math.random() * 11) + 2);
             bannerNode.attr('style','background-image:url(/banner/'+index+'.jpg)');
         }
     })
